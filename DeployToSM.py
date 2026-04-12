e74354715ae7cf82becd15a200c4e5b62ac697f5
Alec Figueroa <afigueroa17@neiu.edu>
2026-04-12 02:40:38 -0500

    deployed to SageMaker

--- DeployToSM.py
+++ DeployToSM.py
@@ -25,12 +25,16 @@ print(f"training on {TRAIN_DEVICE}")
 print(f"deploying on {DEPLOY_DEVICE}")
 
 
-# main()
+#main()
 
 code_dir = os.path.join(LOCAL_MODEL_DIR, 'code')
 if os.path.exists('src/inference.py'):
     shutil.copy('src/inference.py', os.path.join(code_dir, 'inference.py'))
-    shutil.copy('requirements.txt', os.path.join(code_dir, 'requirements.txt'))
+    #shutil.copy('requirements.txt', os.path.join(code_dir, 'requirements.txt'))
+    
+# copy estate_insight.pth into the model folder
+if os.path.exists(MODEL_PATH):
+  shutil.copy(MODEL_PATH, os.path.join(LOCAL_MODEL_DIR, MODEL_PATH))
     
 with tarfile.open(TAR_NAME, 'w:gz') as tar:
   tar.add(MODEL_PATH, arcname='estate_insight.pth')
@@ -64,15 +68,22 @@ print(f"Uploaded model to {s3_model_path}")
 pytorch_model = PyTorchModel(
   model_data=s3_model_path,
   role=role,
-  framework_version='2.0.0',
+  framework_version='2.1.0',
   py_version='py310',
   entry_point='inference.py',
-  sagemaker_session=session
+  sagemaker_session=session,
+  source_dir='model/code/',
+  env={
+        'TS_MAX_RESPONSE_WAIT_TIME': '300',      # TorchServe timeout (5 mins)
+        'SAGEMAKER_MODEL_SERVER_TIMEOUT': '300', # SageMaker container timeout
+        'MODEL_SERVER_TIMEOUT': '300',
+        'SAGEMAKER_MODEL_SERVER_WORKERS': '1'
+    }
 )
 
 predictor = pytorch_model.deploy(
   initial_instance_count=1,
-  instance_type=DEPLOY_DEVICE,
+  instance_type=TRAIN_DEVICE,
   serializer=DataSerializer(content_type="image/jpeg"),
   deserializer=JSONDeserializer()
 )
@@ -80,6 +91,7 @@ predictor = pytorch_model.deploy(
 # Find a test image and send it to same directory as this script, name it 'img.jpg'
 img_pth = 'img.jpg'
 image = Image.open(img_pth).convert('RGB')
+# can't send PIL images to our model in the cloud, must send bytes instead
 buffer = io.BytesIO()
 # You must specify a format (e.g., JPEG or PNG) to compress the object into bytes
 image.save(buffer, format="JPEG") 
--- model/code/inference.py
+++ model/code/inference.py
@@ -1,20 +1,82 @@
-from .EstateInsightModel import EstateInsightModel
 from PIL import Image
 from torchvision import transforms
+from collections import OrderedDict
 import torch.nn as nn
+import torchvision.models as models
 import torch
 import json
 import os
 import io
 
+class EstateInsightModel(nn.Module):
+    def __init__(self, num_quality_classes, num_type_classes):
+        super(EstateInsightModel, self).__init__()
+        # Load a pre-trained ResNet model
+        self.model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        # Replace the final fully connected layer to match the number of classes
+        in_features = self.model.fc.in_features
+
+        # Freeze all layers except the final fully connected layer
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # remove original fully connected layer
+        self.model.fc = nn.Identity()
+        
+        # Two separate outputs for the quality and type classifications
+        self.quality_head = nn.Linear(in_features, num_quality_classes)
+        self.type_head = nn.Linear(in_features, num_type_classes)
+        
+        for name, param in self.model.named_parameters():
+            if "layer4" in name or "fc" in name or 'layer3' in name:
+                param.requires_grad = True
+
+    def forward(self, x):
+        features = self.model(x)
+        quality_output = self.quality_head(features)
+        type_output = self.type_head(features)
+        return quality_output, type_output
+
 def model_fn(model_dir):
-  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-  model = EstateInsightModel()
-  model_path = os.path.join(model_dir, 'estate_insight.pth')
-  with(open(model_path, 'rb') as f):
-    model.load_state_dict(torch.load(f, map_location=device))
-  
-  return model.to(device)
+  print("---1. Entering model_fn---")
+  try:
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"---2. Device: {device}---")
+    
+    num_quality_classes, num_type_classes = 3, 5
+    model = EstateInsightModel(num_quality_classes, num_type_classes)
+    print(f"---3. Class Initialized---")
+    
+    model_path = os.path.join(model_dir, 'estate_insight.pth')
+    checkpoint = torch.load(model_path, map_location=device)
+    print("---4. Checkpoint Loaded---")
+   # 3. Pull the specific state_dict key you used during save
+    state_dict = checkpoint.get('model_state_dict', checkpoint)
+
+    # 4. Clean 'module.' prefixes if you trained with DataParallel
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k[7:] if k.startswith('module.') else k
+        new_state_dict[name] = v
+
+    # 5. Load with strict=False to ignore minor version/metadata differences
+    model.load_state_dict(new_state_dict, strict=False)
+    
+    model.to(device)
+    model.eval()
+    return model
+    
+  #   if 'model_state_dict' in checkpoint:
+  #       model.load_state_dict(checkpoint['model_state_dict'])
+  #   else:
+  #     model.load_state_dict(checkpoint)
+      
+  #   model.to(device)
+  #   model.eval()
+  #   return model
+  except Exception as e:
+    print(f"FAILED TO LOAD MODEL {str(e)}") 
+    raise e
 
 def input_fn(request_body, request_content_type):
   
@@ -25,16 +87,15 @@ def input_fn(request_body, request_content_type):
     return torch.tensor(data, dtype=torch.float32).view(-1, 1)
   
   if request_content_type == 'image/jpeg':
-    image = Image.open(io.BytesIO(request_body).convert("RGB"))
+    image = Image.open(io.BytesIO(request_body)).convert("RGB")
     preprocess = transforms.Compose([
       transforms.Resize(256),
       transforms.CenterCrop(224),
       transforms.ToTensor(),
       transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-    #return preprocess(image).unsqueeze(0)
-    return preprocess(image)
-  
+    return preprocess(image).unsqueeze(0)
+    
   raise ValueError(f"Unsupported format: {request_content_type}")
 
 def predict_fn(input_data, model):
@@ -45,13 +106,19 @@ def predict_fn(input_data, model):
     
     model.eval()
     with torch.no_grad():
-      prediction = model(input_data)
-    return prediction.cpu().numpy()
+      prediction1, prediction2 = model(input_data)
+    result1 = [float(x) for x in prediction1.cpu().numpy().flatten()]
+    result2 = [float(x) for x in prediction2.cpu().numpy().flatten()]
+    #return {"test": [1.0, 2.0], "status": "ok"}
+    print(f"quality: {result1}, type: {result2}")
+    return {"Quality" : result1, "Type": result2}
   
   except Exception as e:
     print(f"Exception occured: {e}")
     raise e
 
-def output_fn(prediction, content_type):
-  return json.dumps(prediction.tolist())
+def output_fn(prediction, accept):
+  if accept == 'application/json':
+    return json.dumps(prediction), accept
+  raise ValueError(f"unsupported accept type: {accept}")
   
